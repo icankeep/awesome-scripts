09cd82f5348843a3102e4fa9c231c7ed5ff0511b
passer <whzhoua@gmail.com>
2023-10-28 15:38:42 +0800

    Update word_convert.go

--- go/word_convert.go
+++ go/word_convert.go
@@ -1,47 +1,46 @@
 package word
 
 import (
-    "strings"
-    "unicode"
+	"strings"
+	"unicode"
 )
 
-//单词全部转化为大写
+// ToUpper 单词全部转化为大写
 func ToUpper(s string) string {
-    return strings.ToUpper(s)
+	return strings.ToUpper(s)
 }
 
-//单词全部转化为小写
+// ToLower 单词全部转化为小写
 func ToLower(s string) string {
-    return strings.ToLower(s)
+	return strings.ToLower(s)
 }
 
-//下划线单词转为大写驼峰单词
-func UderscoreToUpperCamelCase(s string) string {
-    s = strings.Replace(s, "_", " ", -1)
-    s = strings.Title(s)
-    return strings.Replace(s, " ", "", -1)
+// UnderscoreToUpperCamelCase 下划线单词转为大写驼峰单词
+func UnderscoreToUpperCamelCase(s string) string {
+	s = strings.Replace(s, "_", " ", -1)
+	s = strings.Title(s)
+	return strings.Replace(s, " ", "", -1)
 }
 
-//下划线单词转为小写驼峰单词
-func UderscoreToLowerCamelCase(s string) string {
-    s = UderscoreToUpperCamelCase(s)
-    return string(unicode.ToLower(rune(s[0]))) + s[1:]
-    return s
+// UnderscoreToLowerCamelCase 下划线单词转为小写驼峰单词
+func UnderscoreToLowerCamelCase(s string) string {
+	s = UnderscoreToUpperCamelCase(s)
+	return string(unicode.ToLower(rune(s[0]))) + s[1:]
 }
 
-//驼峰单词转下划线单词
-func CamelCaseToUdnderscore(s string) string {
-    var output []rune
-    for i, r := range s {
-        if i == 0 {
-            output = append(output, unicode.ToLower(r))
-        } else {
-            if unicode.IsUpper(r) {
-                output = append(output, '_')
-            }
+// CamelCaseToUnderscore 驼峰单词转下划线单词
+func CamelCaseToUnderscore(s string) string {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			output = append(output, unicode.ToLower(r))
+		} else {
+			if unicode.IsUpper(r) {
+				output = append(output, '_')
+			}
 
-            output = append(output, unicode.ToLower(r))
-        }
-    }
-    return string(output)
+			output = append(output, unicode.ToLower(r))
+		}
+	}
+	return string(output)
 }
